5c8c8f8744eea8ea93ebceabeeb3db23cc11f32c
MeitalHilf <tool16289@gmail.com>
2025-08-06 19:16:35 +0300

    Add GET /activity/list to display all tasks

--- routes/activity_R.js
+++ routes/activity_R.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../db');
 
 // בדיקה פשוטה שהראוטר עובד
-router.get('/list', (req, res) => {
-    res.send("רשימת המשימות תופיע כאן בהמשך");
+router.get('/list', async (req, res) => {
+    try {
+        const [tasks] = await db.query('SELECT * FROM tasks');
+        res.render('activity_list', { tasks });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error retrieving tasks');
+    }
 });
 
 module.exports = router;
