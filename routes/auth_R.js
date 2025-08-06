b711de8986d6a800358625a003a139d7a6ab7eb5
MeitalHilf <tool16289@gmail.com>
2025-08-06 03:35:30 +0300

    feat: handle login form submission and validate user

--- routes/auth_R.js
+++ routes/auth_R.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const { CheckLogin } = require("../Middleware/user_Mid");
 // GET /login
 router.get("/", (req, res) => {
     res.render("login");
+});
+
+router.post("/", CheckLogin, (req, res) => {
+    if (req.validUser){
+        res.send(`שלום, ${req.user.username}! תפקידך הוא: ${req.user.role}`);
+    } else {
+        res.status(401).send("שם משתמש או סיסמה שגויים");
+    }
 });
 
 module.exports = router;
