90efe9751cc9ea097473dc9ac893660d5917fc72
MeitalHilf <tool16289@gmail.com>
2025-08-07 12:39:42 +0300

    created task_Mid.js

---
+++ Middleware/task_Mid.js
@@ -0,0 +1,13 @@
+// Middleware/task_Mid.js
+
+function addTask(req, res) { /* ... */ }
+function listTasks(req, res) { /* ... */ }
+function editTask(req, res) { /* ... */ }
+function deleteTask(req, res) { /* ... */ }
+
+module.exports = {
+    addTask,
+    listTasks,
+    editTask,
+    deleteTask
+};
