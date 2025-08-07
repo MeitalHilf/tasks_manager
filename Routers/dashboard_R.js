4acb373f8c430bc7c2245c0e28ad409813281991
MeitalHilf <tool16289@gmail.com>
2025-08-07 20:56:44 +0300

    Add middleware import to dashboard routes

--- Routers/dashboard_R.js
+++ Routers/dashboard_R.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const user_Mid = require('../middleware/user_Mid');
 
 router.get('/', (req, res) => {
     // if (!req.user) {
