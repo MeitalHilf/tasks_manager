f222fa3be996baeba50804204a592e539ba0e3ed
MeitalHilf <tool16289@gmail.com>
2025-08-07 12:40:45 +0300

    created category_Mid.js

---
+++ Middleware/category_Mid.js
@@ -0,0 +1,13 @@
+// Middleware/category_Mid.js
+
+function addCategory(req, res) { /* ... */ }
+function listCategories(req, res) { /* ... */ }
+function editCategory(req, res) { /* ... */ }
+function deleteCategory(req, res) { /* ... */ }
+
+module.exports = {
+    addCategory,
+    listCategories,
+    editCategory,
+    deleteCategory
+};
