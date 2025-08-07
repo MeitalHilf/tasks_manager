07bba4c5303eb1837d1a859542e30b6586bfb498
MeitalHilf <tool16289@gmail.com>
2025-08-07 14:20:40 +0300

    Add routes for task pages with middleware integration

--- Routes/tasks_R.js
+++ Routes/tasks_R.js
@@ -1,16 +1,71 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const { AddTask, EditTask,ShowTasks,DeleteTask} = require('../Middleware/task_Mid');
 
-// בדיקה פשוטה שהראוטר עובד
-router.get('/list', async (req, res) => {
-    try {
-        const [tasks] = await db.query('SELECT * FROM tasks');
-        res.render('activity_list', { tasks });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Error retrieving tasks');
-    }
+
+// main for tasks pages
+router.get('/', (req, res) => {
+    // if (!req.user) return res.redirect('/');
+
+    res.render('tasks_menu', {
+        user: req.user
+    });
+});
+
+
+
+//  GET add tasks pages
+router.get('/add', (req, res) => {
+    // if (!req.user) return res.redirect('/');
+    res.render('tasks_add', {
+        user: req.user
+    });
+});
+
+
+// שליחת טופס הוספה
+router.post('/add', AddTask, (req, res) => {
+
+});
+
+
+
+
+//  GET Edit tasks pages
+router.get('/edit', (req, res) => {
+    // if (!req.user) return res.redirect('/');
+    res.render('tasks_edit', {
+        user: req.user
+    });
+});
+
+// שליחת טופס עריכה
+router.post('/edit', EditTask, (req, res) => {
+
+});
+
+
+
+router.get('/list', ShowTasks, (req, res) => {
+    // if (!req.user) return res.redirect('/');
+    res.render('tasks_list', {
+        user: req.user,
+        tasks: req.tasks
+    });
+});
+
+
+
+
+// תצוגת טופס מחיקה
+router.get('/delete', (req, res) => {
+    // if (!req.user) return res.redirect('/');
+    res.render('tasks_delete');
+});
+
+// שליחת בקשת מחיקה
+router.post('/delete', DeleteTask, (req, res) => {
+
 });
 
 module.exports = router;
