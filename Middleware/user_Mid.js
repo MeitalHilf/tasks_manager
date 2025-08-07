620d67c76e3b6d5ffe9acd2c42b26bb947f57666
MeitalHilf <tool16289@gmail.com>
2025-08-07 13:42:16 +0300

    Standardized user login middleware to match project structure, improved validation and error handling

--- Middleware/user_Mid.js
+++ Middleware/user_Mid.js
@@ -1,24 +1,26 @@
 const md5 = require('md5');
 
-const pool = require("../db");
-
 async function CheckLogin(req, res, next) {
+    const uname = (req.body.uname  !== undefined) ? addSlashes(req.body.uname     ) : "";
+    const passwd = (req.body.passwd !== undefined) ?            req.body.passwd      : "";
+    const enc_pass = md5("A"+passwd);
+    const Query = `SELECT * FROM users WHERE uname = '${uname}' AND passwd = '${enc_pass}'`;
 
-    console.log("BODY:", req.body);
-    const uname = req.body.uname;
-    const passwd = req.body.passwd;
+
+    const promisePool = db_pool.promise();
+
 
     //test
-    console.log("מנסה לבדוק שם וסיסמה:", uname, passwd);
+    //console.log("מנסה לבדוק שם וסיסמה:", uname, passwd);
 
-    const enc_pass = md5("A" + passwd);
+
     try {
-        const [rows] = await pool.query("SELECT * FROM users WHERE username = ?", [uname]);
+        const [rows] = await promisePool.query(Query); // objects in arr and result from Query in index [0]
 
         console.log("תוצאה מהמסד:", rows);
 
 
-        if (rows.length > 0 && rows[0].password === enc_pass){
+        if (rows.length > 0){
             req.validUser = true;
             req.user = rows[0]; // שומרים את כל הנתונים של המשתמש
         } else {
@@ -26,6 +28,7 @@ async function CheckLogin(req, res, next) {
         }
 
         next();
+
     } catch (error) {
         console.error("Database error:", error);
         res.status(500).send("שגיאה במסד הנתונים");
