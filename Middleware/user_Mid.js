6746f42bd1c7ac69480a29e17f3529ea4d77be01
MeitalHilf <tool16289@gmail.com>
2025-08-07 20:54:10 +0300

    add Middleware/user_Mid.js

--- Middleware/user_Mid.js
+++ Middleware/user_Mid.js
@@ -1,38 +1,64 @@
 const md5 = require('md5');
+const jwt = require('jsonwebtoken');
+
+async function isLogged(req, res, next) {
+    const jwtToken = req.cookies.ImLoggedToYoman;
+    let user_id = -1;
+
+    if (jwtToken !== "") {
+        jwt.verify(jwtToken, 'myPrivateKey', async (err, decodedToken) => {
+            if (err) {
+                console.log("Token error:", err);
+            } else {
+                let data = decodedToken.data;
+                user_id = data.split(",")[0];
+                req.user_id = user_id;
+            }
+        });
+    }
+
+    if (user_id < 0) {
+        return res.redirect("/");
+    }
+
+    next();
+}
+
 
 async function CheckLogin(req, res, next) {
-    const uname = (req.body.uname  !== undefined) ? addSlashes(req.body.uname     ) : "";
-    const passwd = (req.body.passwd !== undefined) ?            req.body.passwd      : "";
-    const enc_pass = md5("A"+passwd);
-    const Query = `SELECT * FROM users WHERE uname = '${uname}' AND passwd = '${enc_pass}'`;
+    const uname = (req.body.uname  !== undefined) ? addSlashes(req.body.uname) : "";
+    const passwd = (req.body.passwd !== undefined) ? req.body.passwd      : "";
+    const Query = `SELECT * FROM users WHERE username = '${uname}' AND password = '${passwd}'`;
 
 
     const promisePool = db_pool.promise();
 
 
-    //test
-    //console.log("מנסה לבדוק שם וסיסמה:", uname, passwd);
 
-
+    let rows=[];
     try {
-        const [rows] = await promisePool.query(Query); // objects in arr and result from Query in index [0]
-
-        console.log("תוצאה מהמסד:", rows);
-
-
-        if (rows.length > 0){
-            req.validUser = true;
-            req.user = rows[0]; // שומרים את כל הנתונים של המשתמש
-        } else {
-            req.validUser = false;
-        }
-
-        next();
-
-    } catch (error) {
-        console.error("Database error:", error);
-        res.status(500).send("שגיאה במסד הנתונים");
+        [rows] = await promisePool.query(Query);
+    } catch (err) {
+        console.log(err);
     }
+
+
+    if(rows.length > 0){
+        req.validUser = true;
+        let val = `${rows[0].id},${rows[0].name}`;
+        let token = jwt.sign(
+            {data: val},
+            'myPrivateKey',
+            { expiresIn: 31*24*60*60 // in sec
+            });
+        res.cookie("ImLoggedToYoman", token, {
+            maxAge: 31*24*60*60 * 1000, // 3hrs in ms
+        });
+
+    }else {
+        req.validUser = false;
+    }
+    next();
 }
 
 module.exports = { CheckLogin };
