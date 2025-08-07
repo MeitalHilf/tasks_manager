8f9f07772928330e944c69b60c09038668a955b1
MeitalHilf <tool16289@gmail.com>
2025-08-07 13:14:42 +0300

    added slashes and updated route paths

--- index.js
+++ index.js
@@ -13,10 +13,16 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-
+let db_M =require('./db');
+global.db_pool = db_M.pool;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+
+global.addSlashes    = require('slashes').addSlashes;
+global.stripSlashes  = require('slashes').stripSlashes;
+
 
 app.use(express.static(path.join(__dirname, "public")));
 
